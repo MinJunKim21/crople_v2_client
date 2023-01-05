5c4376b6465dede7fb392f43fa7af96ae1044701
MinJunKim21 <jinju8012@gmail.com>
2023-01-05 22:26:50 +0900

    [FIXING]개발 배포 환경 비교중, environment variable 설정함

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,6 +27,7 @@ function Login() {
         <button onClick={google}>google</button>
         <button onClick={kakao}>kakao</button>
         <button onClick={naver}>naver</button>
+        <span>{process.env.REACT_APP_MODE}</span>
         <span>{process.env.REACT_APP_HOME_URL}</span>
         <span>{process.env.REACT_APP_API_ROOT}</span>
       </div>
