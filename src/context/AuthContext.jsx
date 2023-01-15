5c5d3f03b4516a936dea751e4038b147b0496573
MinJunKim21 <jinju8012@gmail.com>
2023-01-15 14:31:28 +0900

    [FIXING] 배포에서 로그인이 되는데, getuser api는 존재하지만 값을 못 가져오는 상태, 어디 오류인지 찾는중

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -9,31 +9,31 @@ export default function Context({ children }) {
   const [userObject, setUserObject] = useState({});
   console.log(`${process.env.REACT_APP_API_ROOT}/getuser`);
   console.log(userObject, 'authContext');
-  // useEffect(() => {
-  //   axios
-  //     .get(`${process.env.REACT_APP_API_ROOT}/getuser`, {
-  //       withCredentials: true,
-  //     })
-  //     .then((res) => {
-  //       console.log(res, 'got res');
-  //       if (res.data) {
-  //         console.log(res.data, 'res.data 존재');
-  //         setUserObject(res.data);
-  //         console.log('did set object');
-  //       }
-  //     });
-  // }, [userObject._id]);
-
   useEffect(() => {
-    const getUserObject = async () => {
-      const res = await axios.get(`${process.env.REACT_APP_API_ROOT}/getuser`, {
+    axios
+      .get('http://localhost:5001/getuser', {
         withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res, 'got res');
+        if (res.data) {
+          console.log(res.data, 'res.data 존재');
+          setUserObject(res.data);
+          console.log('did set object');
+        }
       });
-      setUserObject(res.data);
-      console.log(res, 'res');
-    };
-    getUserObject();
   }, [userObject._id]);
+
+  // useEffect(() => {
+  //   const getUserObject = async () => {
+  //     const res = await axios.get(`${process.env.REACT_APP_API_ROOT}/getuser`, {
+  //       withCredentials: true,
+  //     });
+  //     setUserObject(res.data);
+  //     console.log(res, 'res');
+  //   };
+  //   getUserObject();
+  // }, [userObject._id]);
 
   return (
     <AuthContext.Provider value={userObject}>{children}</AuthContext.Provider>
