572b6b8c2ba36494d8df7f2429e9bcb9232a9d0a
MinJunKim21 <jinju8012@gmail.com>
2023-01-26 12:02:09 +0900

    [FIXING]ui 반영 배포 오류, ui 추가 반영중

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -21,6 +21,7 @@ export default function Profile() {
   const [followed, setFollowed] = useState(
     userObject.followings.includes(user?._id)
   );
+  console.log(followed);
   // const [profileChange, setProfileChange] = useState(false);
   // console.log(nickName, 'nickName');
   // console.log(user, 'user');
@@ -62,12 +63,12 @@ export default function Profile() {
   //   window.location.reload(); // 원인 알게되면 이거 바꾸기...
   // };
   return (
-    <div className="w-full h-full">
+    <div>
       {/* <Topbar /> */}
       <BgWrapper>
         {/* <Sidebar /> */}
         <div>
-          <div className="min-w-screen min-h-screen">
+          <div className="min-w-screen min-h-screen relative">
             <div className="flex items-center px-4 pt-11">
               <Link to="/">
                 <BsChevronLeft className="text-2xl h-11 " />
@@ -81,24 +82,34 @@ export default function Profile() {
                 className="w-6"
               />
             </div> */}
-            <div className="bg-white h-full backdrop-blur-[2px]	rounded-t-3xl opacity-95">
-              <h3>프로필</h3>
-              <div>
-                <span>desc : </span>
-                <span>{user.desc}</span>
+            <div className="bg-white w-full backdrop-blur-[2px]	rounded-t-3xl opacity-95 absolute left-0 bottom-0">
+              <div className="flex py-4 text-center w-full">
+                <h3 className="w-full">프로필수정</h3>
+                <h3 className="w-full">미리보기</h3>
               </div>
-              <div>
-                <span>nickName : </span>
-                <span>{user.nickName}</span>
+              <div className="w-[10.5rem] h-[10.5rem] border-red-500 rounded-full border-2 justify-center mx-auto mt-6 mb-14">
+                <div className="justify-center flex items-center h-full w-full">
+                  프사위치
+                </div>
               </div>
 
-              <div>
-                <span>likeSports : </span>
-                <span>{user.likeSports}</span>
-              </div>
-              <div>
-                <span>locations : </span>
-                <span>{user.locations}</span>
+              <div className="mx-4">
+                <div>
+                  <h4 className="text-[#8B8B8B] text-2xl">{user.nickName}</h4>
+                </div>
+                <div>
+                  <span>desc : </span>
+                  <span>{user.desc}</span>
+                </div>
+
+                <div>
+                  <span>likeSports : </span>
+                  <span>{user.likeSports}</span>
+                </div>
+                <div>
+                  <span>locations : </span>
+                  <span>{user.locations}</span>
+                </div>
               </div>
             </div>
           </div>
