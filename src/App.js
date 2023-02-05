6df261d1b89c833b7b21e16d7efbbe55559f9594
MinJunKim21 <jinju8012@gmail.com>
2023-02-05 14:20:34 +0900

    [UPDATE]프로필 사진 등록시 이미지 파일만 선택가능하도록 지정

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { AuthContext } from './context/AuthContext';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 import Messenger from './pages/Messenger';
+import Test from './pages/Test';
 // import NeedProfile from './components/NeedProfile';
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
           path="/messenger"
           element={userObject._id ? <Messenger /> : null}
         />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
