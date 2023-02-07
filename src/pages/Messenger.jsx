2788172a652f802c3b52a3bd4dac3e70c6c67b30
MinJunKim21 <jinju8012@gmail.com>
2023-02-07 18:34:32 +0900

    [FIXING] socket이 끊겨서 로컬로 돌려봄

--- src/pages/Messenger.jsx
+++ src/pages/Messenger.jsx
@@ -26,7 +26,7 @@ export default function Messenger() {
   const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
-    socket.current = io(process.env.REACT_APP_API_ROOT);
+    socket.current = io('ws://localhost:5001');
     socket.current.on('getMessage', (data) => {
       setArrivalMessage({
         sender: data.senderId,
