1f75f64bd4b1756c1add1c3208878f79d4e0ae2a
MinJunKim21 <jinju8012@gmail.com>
2023-02-10 14:00:33 +0900

    [UPDATE] 프로필카드의 모습이 본인의 프로필은 수정모드가 지원되게, 상대방은 매칭으로 컴포넌트 나눔

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,10 +12,11 @@ import { AuthContext } from '../context/AuthContext';
 // import Infoedit from '../components/Infoedit';
 
 import { BsChevronLeft } from 'react-icons/bs';
+import { MyProfileCard } from '../components/MyProfileCard';
+import { UsersProfileCard } from '../components/UsersProfileCard';
 
 export default function Profile() {
   const userObject = useContext(AuthContext);
-  // const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState('');
   const _id = useParams()._id;
   const [followed, setFollowed] = useState(
@@ -64,76 +65,7 @@ export default function Profile() {
   // };
   return (
     <div>
-      {/* <Topbar /> */}
-      <BgWrapper>
-        {/* <Sidebar /> */}
-        <div>
-          <div className="min-w-screen min-h-screen relative">
-            <div className="flex items-center px-4 pt-11">
-              <Link to="/">
-                <BsChevronLeft className="text-2xl h-11 " />
-              </Link>
-            </div>
-
-            {/* <div>
-              <img
-                src={`${process.env.REACT_APP_PUBLIC_FOLDER}/${userObject.profilePicture}`}
-                alt=""
-                className="w-6"
-              />
-            </div> */}
-            <div className="bg-white w-full backdrop-blur-[2px]	rounded-t-3xl opacity-95 absolute left-0 bottom-0">
-              <div className="flex py-4 text-center w-full">
-                <h3 className="w-full">프로필수정</h3>
-                <h3 className="w-full">미리보기</h3>
-              </div>
-              <div className="w-[10.5rem] h-[10.5rem] border-red-500 rounded-full border-2 justify-center mx-auto mt-6 mb-14">
-                <div className="justify-center flex items-center h-full w-full">
-                  프사위치
-                </div>
-              </div>
-
-              <div className="px-4 flex w-full">
-                <div className="flex flex-col w-full">
-                  <div>
-                    <h4 className="text-[#8B8B8B] text-2xl">{user.nickName}</h4>
-                  </div>
-                  <div>
-                    <h4 className="text-[#A5A5A5] ">{user.locations}</h4>
-                  </div>
-                </div>
-
-                <div className="flex w-full">
-                  <h4 className="border-2 border-[#C1C1C1] w-20 h-[1.875rem] text-center items-center rounded-full text-[#C1C1C1]">
-                    {user.likeSports}
-                  </h4>
-                </div>
-              </div>
-              <div className="border-2 border-[#DFDFDF] w-full border-t mt-[3.75rem]"></div>
-              <div className=" px-4 mt-8">
-                <div className="w-full h-40 border-2 mb-40">
-                  <span>{user.desc}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* {user.nickName === userObject.nickName && (
-            <button onClick={() => setProfileChange(true)}>수정하기</button>
-          )}
-          <div>{profileChange && <Infoedit />}</div>
-          <div>
-            {nickName === userObject.nickName && <Rightbar user={user} />}
-          </div>
-          <div>
-            {user.username !== userObject.username && (
-              <button onClick={handleClick}>
-                {followed === true ? 'Unfollow' : 'Follow'}
-              </button>
-            )}
-          </div> */}
-        {/* <Rightbar user={user} /> */}
-      </BgWrapper>
+      {user._id === userObject._id ? <MyProfileCard /> : <UsersProfileCard />}
     </div>
   );
 }
