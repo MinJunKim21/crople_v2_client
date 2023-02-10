f24c79ef5b3ad828b3121725aeba9476b075aed7
MinJunKim21 <jinju8012@gmail.com>
2023-02-11 01:35:54 +0900

    [FIXING]ui 오류 수정

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -185,7 +185,7 @@ const BgGraWrapper = styled.div`
 `;
 
 const BgGraWrapperA = tw(BgGraWrapper)`
-w-full h-screen pt-12 max-w-sm flex flex-col `;
+w-full h-screen pt-12 flex flex-col `;
 
 const CardWhiteBg = styled.div`
   background: #ffffff;
