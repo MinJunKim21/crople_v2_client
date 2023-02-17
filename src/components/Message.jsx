d82c759c7b97a6749a718e09682404eb259c22eb
MinJunKim21 <jinju8012@gmail.com>
2023-02-17 15:57:06 +0900

    [UPDATE]메세지 정렬, 최대 크기 수정

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -15,21 +15,21 @@ export default function Message({
         own ? 'flex flex-row-reverse mb-[0.5rem]' : 'flex flex-col mb-[0.5rem]'
       }
     >
-      <div className="flex">
+      <div className={own ? 'flex flex-row-reverse ' : 'flex'}>
         {
           <img
             src={user?.profilePicture[0]}
             alt=""
             className={`w-12 h-12 rounded-full ${
-              own || isSameSender ? 'opacity-0' : ''
-            }`}
+              isSameSender ? 'opacity-0' : ''
+            } ${own && 'hidden'} `}
           />
         }
         <p
           className={
             own
-              ? 'max-w-xs bg-[#F79D00] items-center h-full px-5 py-3 rounded-[1.625rem] rounded-br-none mx-2 text-white break-words  whitespace-normal'
-              : 'max-w-xs bg-[#F7F7F7] items-center h-full px-5 py-3 rounded-[1.625rem] rounded-bl-none mx-2 break-words  whitespace-normal'
+              ? 'max-w-sm bg-[#F79D00] items-center h-full px-5 py-3 rounded-[1.625rem] rounded-br-none mx-2 text-white break-words  whitespace-normal'
+              : 'max-w-sm bg-[#F7F7F7] items-center h-full px-5 py-3 rounded-[1.625rem] rounded-bl-none mx-2 break-words  whitespace-normal'
           }
         >
           {message.text}
