5a54e6b66158ab683a2d404a55d87f66c3774717
MinJunKim21 <jinju8012@gmail.com>
2023-02-26 18:37:35 +0900

    [UPDATE]메인 유저 프사들 배치 ui 반영

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,8 @@ import NeedProfile from '../components/NeedProfile';
 import TabBar from '../components/btn&tab&bar/MainTabBar';
 import { ProfileCard } from '../components/ProfileCard';
 import { Link } from 'react-router-dom';
+import { RecommendEachUser } from '../components/RecommendEachUser';
+import { RecommendUserPlace } from '../components/RecommendUserPlace';
 
 function Home() {
   const [recommendUsers, setRecommendUsers] = useState([]);
@@ -36,7 +38,6 @@ function Home() {
     };
     fetchRecommend();
   }, []);
-
   return (
     <div>
       {userObject.nickName === undefined || userObject.profilePicture === '' ? (
@@ -62,30 +63,133 @@ function Home() {
 
             <div className="relative h-screen w-screen">
               <Link to={`/profile/${userObject._id}`}>
-                <img
-                  src={userObject.profilePicture[0]}
-                  alt=""
-                  className="w-20 h-20 object-cover rounded-full absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
-                />
-              </Link>
-            </div>
-          </div>
-          <div className="z-50">
-            {recommendUsers.map((user) => (
-              // <Link to={`/profile/${user._id}`} key={user._id}>
-              <button key={user._id} onClick={() => handleUserClick(user)}>
-                <div className="w-10 h-10">
+                <div className="w-[5.375rem] h-[5.375rem] object-cover rounded-full absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-gradient-to-b to-[#F79D00] via-[#CABE40] from-[#9AE286] p-[2px]">
                   <img
-                    src={user.profilePicture[0]}
+                    src={userObject.profilePicture[0]}
+                    alt=""
+                    className="w-20 h-20 object-cover rounded-full absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+                  />
+                </div>
+              </Link>
+
+              <div className="absolute z-50 left-[50%] top-[35%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[0])}
+                  className="w-[4.375rem] h-[4.375rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[0]?.profilePicture[0]}
                     alt=""
                     className="w-full h-full object-cover rounded-full"
                   />
-                </div>
-              </button>
-              // </Link>
-            ))}
-          </div>
+                </button>
+              </div>
 
+              <div className="absolute z-50 left-[65%] top-[53%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[1])}
+                  className="w-[4.375rem] h-[4.375rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[1]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+
+              <div className="absolute z-50 left-[20%] top-[52%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[2])}
+                  className="w-[4.375rem] h-[4.375rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[2]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+
+              <div className="absolute z-50 left-[37%] top-[64%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[3])}
+                  className="w-[3.75rem] h-[3.75rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[3]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+
+              <div className="absolute z-50 left-[81%] top-[35%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[4])}
+                  className="w-[3.75rem] h-[3.75rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[4]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+
+              <div className="absolute z-50 left-[18%] top-[29%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[5])}
+                  className="w-[3.75rem] h-[3.75rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[5]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+
+              <div className="absolute z-50 left-[45%] top-[15%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[6])}
+                  className="w-[3.125rem] h-[3.125rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[6]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+
+              <div className="absolute z-50 left-[15%] top-[76%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[7])}
+                  className="w-[3.125rem] h-[3.125rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[7]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+
+              <div className="absolute z-50 left-[77%] top-[74%]">
+                <button
+                  onClick={() => handleUserClick(recommendUsers[8])}
+                  className="w-[3.125rem] h-[3.125rem] border-[#A5A5A5] border-2 rounded-full"
+                >
+                  <img
+                    src={recommendUsers[8]?.profilePicture[0]}
+                    alt=""
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
           <div className="fixed bottom-0 left-[50%] translate-x-[-50%]  w-full pb-8 px-4 max-w-sm mx-auto justify-center">
             <TabBar />
           </div>
