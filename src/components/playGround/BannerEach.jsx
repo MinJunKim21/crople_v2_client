98651f599e37a8a59b8cab01734ff1003820eaf5
MinJunKim21 <jinju8012@gmail.com>
2023-03-06 22:59:00 +0900

    [UPDATE]이미지 슬라이드에 블랙그라데이션 깔음

--- src/components/playGround/BannerEach.jsx
+++ src/components/playGround/BannerEach.jsx
@@ -10,6 +10,11 @@ export const BannerEach = ({ banner }) => {
         className="h-[300px] object-cover w-full"
         alt=""
       />
+      <img
+        src="/assets/pattern/BlackDownGra.png"
+        className="absolute bottom-0 w-full object-contain flex"
+        alt=""
+      />
       <div className="absolute bottom-0 right-0 text-white bg-opacity-50 px-4 py-8 text-left">
         <h3 className="text-2xl font-bold">{banner.name}</h3>
         <p className="text-sm">
--- src/pages/SportsFacility.jsx
+++ src/pages/SportsFacility.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { sportsFacility } from '../data/playGroundData';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper';
 
 export const SportsFacility = () => {
   const { id } = useParams();
@@ -13,7 +17,38 @@ export const SportsFacility = () => {
   }
 
   return (
-    <div>
+    <div className="max-w-md mx-auto">
+      <Swiper
+        style={{
+          '--swiper-pagination-color': '#ffffff',
+          '--swiper-pagination-bullet-inactive-color': '#ffffff',
+          '--swiper-pagination-bullet-inactive-opacity': '0.5',
+          '--swiper-pagination-bullet-size': '6px',
+          '--swiper-pagination-bullet-horizontal-gap': '6px',
+        }}
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        loop={true}
+      >
+        {facility.image.map((img, index) => (
+          <SwiperSlide key={index}>
+            <div>
+              <img
+                key={index}
+                src={img}
+                alt=""
+                className="h-[300px] object-cover w-full"
+              />
+              <img
+                src="/assets/pattern/BlackDownGra.png"
+                className="absolute bottom-0 w-full object-contain flex"
+                alt=""
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <h2>{facility.name}</h2>
       <p>{facility.desc}</p>
       <img src={facility.mainImage} alt={facility.name} />
@@ -23,11 +58,6 @@ export const SportsFacility = () => {
       <p>Price: {facility.price}</p>
       <p>Sub Facilities: {facility.subFacility}</p>
       <h3>Images</h3>
-      <div>
-        {facility.image.map((img, index) => (
-          <img key={index} src={img} alt="" />
-        ))}
-      </div>
     </div>
   );
 };
