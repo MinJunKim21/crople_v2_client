370a3160b3edffedb8cc311b94ab168bf8abe5d4
MinJunKim21 <jinju8012@gmail.com>
2023-03-08 15:27:16 +0900

    [REFACTOR]불필요 코드들 정리

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,21 +10,6 @@ export default function Context({ children }) {
   const [userObject, setUserObject] = useState({});
   const [isLoading, setIsLoading] = useState(true); //eslint-disable-line no-unused-vars
 
-  // useEffect(() => {
-  //   console.log(process.env.REACT_APP_API_ROOT, 'getting authcontext');
-  //   axios
-  //     .get(`${process.env.REACT_APP_API_ROOT}/getuser`, {
-  //       withCredentials: true,
-  //     })
-  //     .then((res) => {
-  //       // console.log(res, 'got res');
-  //       if (res.data) {
-  //         // console.log(res.data, 'res.data 존재');
-  //         setUserObject(res.data);
-  //         // console.log('did set object');
-  //       }
-  //     });
-  // }, [userObject._id]);
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_ROOT}/getuser`, {
