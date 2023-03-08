c09b4ac1a5424c02fbd41019cbed08a67bd0ca69
MinJunKim21 <jinju8012@gmail.com>
2023-03-08 17:43:27 +0900

    [UPDATE]profile edit page 컴포넌트 정리

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -14,6 +14,9 @@ import { ProfileEditHeader } from '../components/profileEdit/ProfileEditHeader';
 import { ProfilePicOne } from '../components/profileEdit/ProfilePicOne';
 import { ProfilePicTwo } from '../components/profileEdit/ProfilePicTwo';
 import { ProfilePicThree } from '../components/profileEdit/ProfilePicThree';
+import { EditCancelCheck } from '../components/profileEdit/EditCancelCheck';
+import { SportsCheck } from '../components/profileEdit/SportsCheck';
+import { LocationsCheck } from '../components/profileEdit/LocationsCheck';
 
 const LOCATION_LIST = [
   { id: 0, data: '마포구' },
@@ -290,167 +293,36 @@ export const ProfileEdit = () => {
         </div>
       )}
       {locationQuestion && (
-        <div className="bg-black bg-opacity-20 w-screen h-screen absolute left-0 top-0 ">
-          <div className="absolute max-w-[21.5rem] w-full top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-white rounded-[2.5rem] p-4">
-            <MainQuestion>운동할 지역을 설정해주세요</MainQuestion>
-            <SubInstruction>중복으로 선택할 수 있어요</SubInstruction>
-            <div className="justify-center grid items-center mt-8">
-              <ul className="grid grid-cols-2 gap-x-2 gap-y-4 ">
-                {LOCATION_LIST.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      <input
-                        id={item.id}
-                        type="checkbox"
-                        value={item.data}
-                        ref={locations}
-                        onChange={(e) => {
-                          onCheckedLocationsElement(
-                            e.target.checked,
-                            e.target.value
-                          );
-                        }}
-                        checked={
-                          locationsCheckedList.includes(item.data)
-                            ? true
-                            : false
-                        }
-                        className="hidden peer"
-                      />
-                      <OptionBtn htmlFor={item.id}>{item.data}</OptionBtn>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            {locationsCheckedList.length === 0 && (
-              <h4 className="text-xs flex pt-4 justify-center text-[#FF2525]">
-                1개 항목 이상 선택해 주세요
-              </h4>
-            )}
-            <div className="w-full h-full flex justify-center space-x-2 pt-8">
-              <button
-                onClick={() => {
-                  setLocationsCheckedList(initialLocationsCheckedList);
-                  setLocationQuestion(false);
-                }}
-                className="bg-[#DFDFDF] w-28 h-12 rounded-xl"
-              >
-                취소
-              </button>
-              {locationsCheckedList.length === 0 ? (
-                <div className="bg-[#F79D00] flex justify-center items-center text-white w-28 h-12 rounded-xl">
-                  선택완료
-                </div>
-              ) : (
-                <button
-                  onClick={() => {
-                    setLocationQuestion(false);
-                    setTempLocation(true);
-                  }}
-                  className="bg-[#F79D00] text-white w-28 h-12 rounded-xl"
-                >
-                  선택완료
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
+        <LocationsCheck
+          LOCATION_LIST={LOCATION_LIST}
+          onCheckedLocationsElement={onCheckedLocationsElement}
+          locations={locations}
+          setLocationsCheckedList={setLocationsCheckedList}
+          initialLocationsCheckedList={initialLocationsCheckedList}
+          setLocationQuestion={setLocationQuestion}
+          setTempLocation={setTempLocation}
+          locationsCheckedList={locationsCheckedList}
+        />
       )}
       {sportsQuestion && (
-        <div className="bg-black bg-opacity-20 w-screen h-screen absolute left-0 top-0 ">
-          <div className="absolute max-w-[21.5rem] w-full top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-white rounded-[2.5rem] p-4">
-            <MainQuestion>어떤 운동을 좋아하세요?</MainQuestion>
-            <SubInstruction>다섯 개까지 선택할 수 있어요</SubInstruction>
-            <div className="justify-center grid items-center mt-8">
-              <ul className="grid grid-cols-2 gap-x-2 gap-y-4 ">
-                {SPORTS_LIST.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      <input
-                        id={item.id}
-                        type="checkbox"
-                        className="hidden peer"
-                        value={item.data}
-                        ref={likeSports}
-                        onChange={(e) => {
-                          onCheckedSportsElement(
-                            e.target.checked,
-                            e.target.value
-                          );
-                        }}
-                        checked={
-                          sportsCheckedList.includes(item.data) ? true : false
-                        }
-                      />
-                      <OptionBtn htmlFor={item.id}>{item.data}</OptionBtn>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            {sportsCheckedList.length === 0 && (
-              <h4 className="text-xs flex pt-4 justify-center text-[#FF2525]">
-                1개 항목 이상 선택해 주세요
-              </h4>
-            )}
-            <div className="w-full h-full flex justify-center space-x-2 pt-8">
-              <button
-                onClick={() => {
-                  setSportsCheckedList(initialSportsCheckedList);
-                  setSportsQuestion(false);
-                }}
-                className="bg-[#DFDFDF] w-28 h-12 rounded-xl "
-              >
-                취소
-              </button>
-              {sportsCheckedList.length === 0 ? (
-                <div className="bg-[#F79D00] items-center justify-center flex text-white w-28 h-12 rounded-xl">
-                  선택완료
-                </div>
-              ) : (
-                <button
-                  onClick={() => {
-                    setSportsQuestion(false);
-                    setTempSports(true);
-                  }}
-                  className="bg-[#F79D00] text-white w-28 h-12 rounded-xl"
-                >
-                  선택완료
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
+        <SportsCheck
+          SPORTS_LIST={SPORTS_LIST}
+          likeSports={likeSports}
+          onCheckedSportsElement={onCheckedSportsElement}
+          sportsCheckedList={sportsCheckedList}
+          setSportsQuestion={setSportsQuestion}
+          setSportsCheckedList={setSportsCheckedList}
+          initialSportsCheckedList={initialSportsCheckedList}
+          setTempSports={setTempSports}
+        />
       )}
       {cancelEdit && (
-        <div className="bg-black bg-opacity-20 w-screen h-screen absolute left-[50%] top-0 max-w-md  translate-x-[-50%]  ">
-          <div className="absolute max-w-[21.5rem] w-full top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] bg-white rounded-[2.5rem] p-4">
-            <MainQuestion>작성을 취소할까요?</MainQuestion>
-            <SubInstruction>저장되지 않은 내용은 삭제됩니다</SubInstruction>
-            <div className="w-full h-full flex justify-center space-x-2 pt-8">
-              <button
-                onClick={() => {
-                  setCancelEdit(false);
-                }}
-                className="bg-[#DFDFDF] w-28 h-12 rounded-xl "
-              >
-                아니요
-              </button>
-              <Link to={`/profile/${userObject._id}`}>
-                <button
-                  onClick={() => {
-                    setSportsQuestion(false);
-                    setTempSports(true);
-                  }}
-                  className="bg-[#F79D00] text-white w-28 h-12 rounded-xl"
-                >
-                  네
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
+        <EditCancelCheck
+          setCancelEdit={setCancelEdit}
+          setSportsQuestion={setSportsQuestion}
+          setTempSports={setTempSports}
+          userObject={userObject}
+        />
       )}
     </form>
   );
